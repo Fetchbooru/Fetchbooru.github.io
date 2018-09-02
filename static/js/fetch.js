3677c85c7ae3bf9a9f6bfe00bac6959db3649e92
BlueReptile <callmekalango@gmail.com>
2018-09-02 20:29:45 -0300

    giphy support...
    please dont use my apikey, respect opensource stuff.

--- static/js/fetch.js
+++ static/js/fetch.js
@@ -75,13 +75,16 @@ function setNotSafe() {
   md5s = [];
   pageLoad();
   repeats = 0;
+  offset = 0;
 }
 
 var md5s = [];
 var page = 1;
+var offset = 0;
 
 function nextPage() {
   page++;
+  offset += 25;
   pageLoad();
 }
 
@@ -101,11 +104,94 @@ function pageLoad() {
   /*if($('#checkGel').is(':checked')){
     updateGel();
     }*/
+  if ($('#checkGif').is(':checked')) {
+    updateGif();
+  }
 }
 
 function clearBox() {
   $grid.masonry('remove', $grid.find('.block'));
 }
+
+//
+
+function updateGif() {
+  $.ajax({
+    type: 'GET',
+    url: 'https://api.giphy.com/v1/gifs/search?api_key=LblcJMKjfxA9NteHkwgu5oeSiKbylNXw&q=anime' +
+      ' ' +
+      tag + '&offset=' +
+      offset,
+    data: {
+      get_param: 'value'
+    },
+    dataType: 'json',
+    success: function(data) {
+      if ($.isEmptyObject(data)) {
+        console.log('No more Gif Results');
+        var rip =
+          '<div class="block"><div class="card"><div class="card-image waves-effect waves-block waves-light"><a>No more Giphy Results</a></div><div class="card-content"><span class="card-title activator grey-text text-darken-4">ID:<i class="material-icons right">more_vert</i></span><p><a rel="noopener noreferrer" href="">Source</a></p></div><div class="card-reveal"><span class="card-title grey-text text-darken-4">ID:<i class="material-icons right">close</i></span><p>Tags: ' +
+          tag + '.</p></div></div>';
+        var $rip = $(rip);
+        $grid.append($rip).masonry('appended', $rip);
+        $grid.masonry();
+        $grid.masonry('layout');
+      }
+      $.each(data.data, function(index, element) {
+        var preview = element.embed_url;
+        var id = element.id;
+        var source = element.url;
+        var md5 = element.id;
+        var safe = element.rating;
+        var tags = element.title;
+        lmd5s = md5s.length;
+        var stuff =
+          '<div class="block"><div class="card"><div class="card-image waves-effect waves-block waves-light"><iframe class="activator"  frameBorder="0" id="img" src="' +
+          preview +
+          '" /></div><div class="card-content"><span class="card-title activator grey-text text-darken-4">ID: ' +
+          id +
+          '<i class="material-icons right">more_vert</i></span><p><a rel="noopener noreferrer" href="' +
+          source +
+          '">Source</a></p></div><div class="card-reveal"><span class="card-title grey-text text-darken-4">ID: ' +
+          id +
+          '<i class="material-icons right">close</i></span><p>Tags: ' +
+          tags + '.</p></div>  </div>';
+        var $stuff = $(stuff);
+        if (safeRate == true) {
+          if (safeRate == true) {
+            if (inArray(md5, md5s)) {
+              repeats++;
+            } else {
+              md5s.push(md5)
+              $grid.append($stuff).masonry('appended', $stuff);
+              $grid.masonry();
+              $grid.masonry('layout');
+            }
+          }
+
+        } else {
+          if (inArray(md5, md5s)) {
+            repeats++;
+          } else {
+            md5s.push(md5)
+            $grid.append($stuff).masonry('appended', $stuff);
+            $grid.masonry();
+            $grid.masonry('layout');
+          }
+        }
+      });
+    },
+    error: function(data) {
+      var stuff =
+        '<p class="infinite-scroll-last center">End of content</p>';
+      var $stuff = $(stuff);
+      $grid.append($stuff).masonry('appended', $stuff);
+      $grid.masonry();
+      $grid.masonry('layout');
+    }
+  });
+}
+
 
 function updateYan() {
   $.ajax({
@@ -472,7 +558,8 @@ $(document.body).on('touchmove', onScroll); // for mobile
 $(window).on('scroll', onScroll);
 
 function onScroll() {
-  if ($(window).scrollTop() + window.innerHeight >= (document.body.scrollHeight - 50)) {
+  if ($(window).scrollTop() + window.innerHeight >= (document.body.scrollHeight -
+      50)) {
     nextPage();
     $grid.masonry();
   }
