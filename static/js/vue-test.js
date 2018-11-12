b769a74e30a882e5a6e821b0f1cf275d97036696
BlueReptile <solowingphanzer@gmail.com>
2018-11-12 02:30:52 -0200

    add danbooru support (again)

--- static/js/vue-test.js
+++ static/js/vue-test.js
@@ -44,10 +44,16 @@ Vue.component('card-grid', {
   // The grid component now accepts a
   // "prop", which is like a custom attribute.
   // This prop is called itens.
-  props: ['grid'],
+  props: {
+    grid:{
+      type:Object
+    },
+},
   template:'<div v-masonry-tile class="block"><div class="card grey darken-3"><div class="card-image waves-effect waves-block waves-light"><img class="activator"  frameBorder="0" id="img" v-bind:src="grid.preview_url" /></div><div class="card-content"><span class="card-title activator white-text">ID:{{grid.id}}<i class="material-icons right">more_vert</i></span><p><a rel="noopener noreferrer" target="_blank" v-bind:href="grid.jpeg_url">Source</a></p></div><div class="card-reveal grey darken-3"><span class="card-title white-text">ID:{{grid.id}}<i class="material-icons right">close</i></span><p class="white-text">Tags:{{grid.tags}}.</p></div></div></div>'
 }
 )
+
+
 var page=0;
 var app = new Vue({
   el: '#app',
@@ -133,6 +139,44 @@ var app = new Vue({
       })
       .catch(e => {
         console.log("Yandere falhou")
+        this.errors.push(e)
+      })
+    },
+    getDan: function() {
+      page++
+      axios.get(`https://danbooru.donmai.us/posts.json?commit=Search&limit=20&page=`+ page +'&tags='+ this.tags, {transformResponse: [function (data) {
+        // Do whatever you want to transform the data
+        // danbooru api doest match konachan/yandere, so I ahve to do some changes in the response data
+        console.log(data);
+        console.log(typeof(data));
+        data = data.replace(/large_file_url/g,"jpeg_url");
+        data = data.replace(/file_url/g,"preview_url");
+        data = data.replace(/tag_string/g,"tags");
+        data = JSON.parse(data);
+        return data;
+      }],})
+      .then(response => {
+        // JSON responses are automatically parsed.
+        console.log("Danbooru pego")
+        console.log(response.data)
+        console.log(typeof(response.data));
+        for(cont=0;cont<20;cont++){
+          console.log("entrei no loop")
+          console.log(response.data[cont]);
+          if(response.data[cont].rating=='s'){//verify if safe for work
+            console.log("encontrei um safe")
+            if(!this.md5s.includes(response.data[cont].md5)){
+              console.log("nao repetido")
+              this.md5s.push(response.data[cont].md5)
+              this.cardList.push(response.data[cont])
+            }else{
+              this.blocks++
+            }
+          }
+        }
+      })
+      .catch(e => {
+        console.log("Danbooru falhou")
         this.errors.push(e)
       })
     },
